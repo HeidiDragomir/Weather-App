55d2a1567e9ff1d19ebd998c1ebfec78b984d1f6
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-04-30 17:52:21 +0200

    Update with forecast component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,60 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
-import Weather from './components/weather/weather'
+import axios from "axios";
+import Weather from './components/weather/weather';
+import Forecast from './components/forecast/forecast';
 
 
 function App() {
 
+
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
-  const [info, setInfo] = useState([]);
-
+  const [data, setData] = useState(null);
+  
   useEffect(() => {
-    const fetchData = async () => {
-      navigator.geolocation.getCurrentPosition(function (position) {
+
+    const getPosition = async () => {
+
+      if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
-      });
+        let finalUrl = `${process.env.REACT_APP_API_URL}lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`;
 
-      await fetch(`${process.env.REACT_APP_API_URL}/forecast/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-        .then(res => res.json())
-        .then(result => {
-          setInfo(result)
-          
-        });
+        getWeather(finalUrl);
+      });}
+
+      else {
+        return alert("Need position to work!")
+      }
+
+      async function getWeather(url) {
+        try {
+          const response = await axios.get(url);
+          setData(response.data);
+        } catch {
+          console.log("error");
+        }
+      }
     }
-    fetchData();
+    getPosition();
+    
   }, [lat, long])
 
+  
 
   return (
     <div className='App'>
-      {(typeof info.list != 'undefined') ? (
-        <Weather weatherData={info} />
-      ) : (
-        <div></div>
+    {data && (
+        <>
+        <Weather
+
+          data={data} />
+        <Forecast
+
+            data={data} />
+            </>
       )}
     </div>
   );
