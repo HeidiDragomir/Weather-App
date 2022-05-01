c05df999181588b229992a255deb7a32473e9aec
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-05-01 19:35:33 +0200

    Add some classNames

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -2,50 +2,52 @@ import React from 'react';
 import './styles.css';
 
 export default function forecast({ data }) {
-    
-  function weatherHourly() {
-    let hourly = []
-    data.hourly.forEach((item, index) => {
-      if(index % 3 === 0 && hourly.length < 6) {
-        hourly.push(<div>
-        <p className="time-hourly">{new Date(item.dt * 1000).toLocaleTimeString('en-IN',{ hour: '2-digit', minute: '2-digit' })}</p>
-        <img src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} alt="Item weather"></img>
-        <p className="main-hourly">{item.weather[0].main}</p>
-        <p>{item.temp.toFixed()}°</p>
-        <p>{item.wind_speed.toFixed()} m/s</p>
-        <p>{item.humidity} %</p></div>)
-      } 
-    })
-    return hourly
 
+  function weatherHourly() {
+    let hourly = [];
+    data.hourly.forEach((item, index) => {
+      if (index % 3 === 0 && hourly.length < 5) {
+        hourly.push(
+        <div className='card-wrapper'>
+          <div className='card-body'>
+            <p className='time'>{new Date(item.dt * 1000).toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' })}</p>
+            <img src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} alt="Weather icon"></img>
+            <p className='info-main'>{item.weather[0].description.toUpperCase()}</p>
+            <p className='info-forecast-temp'>{item.temp.toFixed()}°</p>
+            <p className='info-forecast'>{item.wind_speed.toFixed()} m/s</p>
+            <p className='info-forecast'>{item.humidity} %</p></div></div>)
+      }
+    })
+
+    return hourly
   }
 
   function weatherDaily() {
-    let daily = []
-    data.daily.forEach((item, index) => {
-      if(daily.length < 5) {
-        daily.push(<div>
-        <p className="time">{new Date(item.dt * 1000).toLocaleDateString('en-IN', {month: '2-digit', day: '2-digit'})}</p>
-        <img src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} alt="Item weather"></img>
-        <p className="mainweather">{item.weather[0].main}</p>
-        <p>{item.temp.day.toFixed()}°</p>
-        <p>{item.wind_speed.toFixed()} m/s</p>
-        <p>{item.humidity} %</p>
-        </div>)
-      } 
+    let daily = [];
+    data.daily.forEach((item) => {
+      if (daily.length < 6) {
+        daily.push(
+        <div className='card-wrapper'>
+          <p className='time'>{new Date(item.dt * 1000).toLocaleDateString('en-GB', { weekday: 'short', month: '2-digit', day: '2-digit' })}</p>
+          <img src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} alt="Weather icon"></img>
+          <p className='info-main'>{item.weather[0].main.toUpperCase()}</p>
+          <p className='info-forecast-temp'>{item.temp.day.toFixed()}°</p>
+          <p className='info-forecast'>{item.wind_speed.toFixed()} m/s</p>
+          <p className='info-forecast'>{item.humidity} %</p>
+          </div>)
+      }
     })
     return daily
   }
 
-    return (
-      <>
-      <div className="container-hourly">
+  return (
+    <>
+      <div className="container-hourly ">
         {weatherHourly()}
       </div>
       <div className="container-daily">
-          {weatherDaily()}
-        </div>
-        </>
-    )
-    
+        {weatherDaily()}
+      </div>
+    </>
+  )
 }
